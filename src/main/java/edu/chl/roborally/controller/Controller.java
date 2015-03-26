f46e235dfa7eb51b1cde50f8abe62751a037e13d
Henrik Nilson <henrik.nilson91@gmail.com>
2015-03-26 14:01:28 +0100

    controller fixes

--- src/main/java/edu/chl/roborally/controller/Controller.java
+++ src/main/java/edu/chl/roborally/controller/Controller.java
@@ -1,6 +1,7 @@
 package edu.chl.roborally.controller;
 
 import edu.chl.roborally.model.RoboRally;
+import java.util.Scanner;
 
 public class Controller {
 	private final RoboRally roboRally;
@@ -17,12 +18,43 @@ public class Controller {
         play();
 	}
 
-    private void play(){
-        while(run) {
-            System.out.println("Running");
-            this.run = false;
+    private String userInput() {
+        Scanner in = new Scanner(System.in);
+        System.out.print(">");
+        String s = in.nextLine();
+        return s;
+    }
+
+    private void actionFromInput(String s) {
+        if(s.equals("New Game")) {
+            System.out.println("Starting new game...");
         }
-            System.out.println("Stopped");
+        else if (s.equals("End")) {
+            stopGame();
+        }
+        else if(s.equals("Help")) {
+            System.out.println("Commands: 'New Game', 'End'");
+        }
+        else {
+            System.out.println(s + " not a command");
+        }
+    }
+
+    private void stopGame() {
+        this.run = false;
+    }
+
+    private void play(){
+
+        System.out.println("Running");
+        System.out.println("ROBORALLY MADNESS!!!");
+        System.out.println("type Help if you get stuck!");
+
+        while(run) {
+            actionFromInput(userInput());
+        }
+
+        System.out.println("Game Ended");
     }
 
 }
