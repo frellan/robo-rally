10a02114de205faa2424cab431d0dd08b63bb014
Pertta <parlofqvist@gmail.com>
2015-03-26 12:25:50 +0100

    Created Player class

---
+++ src/main/java/edu/chl/roborally/model/Player.java
@@ -0,0 +1,28 @@
+package edu.chl.roborally.model;
+
+/**
+ * Created by Pertta on 15-03-26.
+ */
+public class Player {
+
+    //Constructor
+    public Player {
+
+    }
+
+    //Queries
+
+    public String getName() {
+
+        return "HankTheTank";
+    }
+
+    public int getLifeTokens() {
+        return 3;
+    }
+
+
+
+    //Commands
+
+}
