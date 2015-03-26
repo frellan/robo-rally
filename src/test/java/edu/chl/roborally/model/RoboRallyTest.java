ef40ce4d33ce37b65ef3bbeb8f04e273ad6a2e56
Henrik Nilson <henrik.nilson91@gmail.com>
2015-03-26 11:55:10 +0100

    and again

--- src/test/java/edu/chl/roborally/model/RoboRallyTest.java
+++ src/test/java/edu/chl/roborally/model/RoboRallyTest.java
@@ -4,16 +4,5 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RoboRallyTest {
-	private static final int NUM_INCREMENTATIONS = 128;
-
-	@Test
-	public void testIncrementResult() {
-		final RoboRally roboRally = new RoboRally();
-
-		for (int i = 0; i < NUM_INCREMENTATIONS; i++) {
-			roboRally.incrementPresses();
-		}
-
-		Assert.assertEquals(NUM_INCREMENTATIONS, roboRally.getPresses());
-	}
+	
 }
