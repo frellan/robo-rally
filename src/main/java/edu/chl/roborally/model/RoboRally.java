0425547e57c83fad55190873dbe5090e756e4287
Henrik Nilson <henrik.nilson91@gmail.com>
2015-03-26 14:32:51 +0100

    public shit

--- src/main/java/edu/chl/roborally/model/RoboRally.java
+++ src/main/java/edu/chl/roborally/model/RoboRally.java
@@ -6,17 +6,17 @@ public class RoboRally {
     private int numbersOfPlayers;
     private ArrayList<Player> players = new ArrayList<Player>();
 
-    protected void newGame() {
+    public void newGame() {
         for (int i = 0; i < numbersOfPlayers; i++) {
             players.add(new Player());
         }
     }
 
-    protected int getNumbersOfPlayers() {
+    public int getNumbersOfPlayers() {
         return numbersOfPlayers;
     }
 
-    protected void setNumbersOfPlayers(int i) {
+    public void setNumbersOfPlayers(int i) {
         this.numbersOfPlayers = i;
     }
 }
