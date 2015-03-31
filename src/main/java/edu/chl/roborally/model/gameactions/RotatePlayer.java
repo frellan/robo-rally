c0cc8cba1b9c290e8364b38b2c7e9ae6177620f8
Henrik Nilson <henrik.nilson91@gmail.com>
2015-03-31 16:30:00 +0200

    Rotate Player started

--- src/main/java/edu/chl/roborally/model/gameactions/RotatePlayer.java
+++ src/main/java/edu/chl/roborally/model/gameactions/RotatePlayer.java
@@ -1,12 +1,59 @@
 package edu.chl.roborally.model.gameactions;
 
+import edu.chl.roborally.model.Constants;
+import edu.chl.roborally.model.Player;
+
 /**
  * Created by henriknilson on 31/03/15.
  */
 public class RotatePlayer implements GameAction {
 
+    private Player p;
+    private Constants.Directions d;
+
+    public RotatePlayer(Player p, Constants.Directions d){
+        this.p = p;
+        this.d = d;
+    }
+
     @Override
     public void action() {
+        switch(d) {
 
+            // Turn Right
+            case EAST:
+                switch (p.getDirection()) {
+                    case NORTH:
+                        p.setDirection(Constants.Directions.EAST);
+                        break;
+                    case EAST:
+                        p.setDirection(Constants.Directions.SOUTH);
+                        break;
+                    case SOUTH:
+                        p.setDirection(Constants.Directions.WEST);
+                        break;
+                    case WEST:
+                        p.setDirection(Constants.Directions.NORTH);
+                }
+                break;
+
+            // Turn Left
+            case WEST:
+                switch (p.getDirection()) {
+                    case NORTH:
+                        p.setDirection(Constants.Directions.WEST);
+                        break;
+                    case WEST:
+                        p.setDirection(Constants.Directions.SOUTH);
+                        break;
+                    case SOUTH:
+                        p.setDirection(Constants.Directions.EAST);
+                        break;
+                    case EAST:
+                        p.setDirection(Constants.Directions.NORTH);
+                        break;
+                }
+                break;
+        }
     }
 }
