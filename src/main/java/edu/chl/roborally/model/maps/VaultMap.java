c67cb976ba0d9735ba2a90487d65b3e07ed2e208
Aringskog <a.aringskog@gmail.com>
2015-04-22 14:34:00 +0200

    added walls to vault map

--- src/main/java/edu/chl/roborally/model/maps/VaultMap.java
+++ src/main/java/edu/chl/roborally/model/maps/VaultMap.java
@@ -1,9 +1,7 @@
 package edu.chl.roborally.model.maps;
 
 import edu.chl.roborally.model.Constants;
-import edu.chl.roborally.model.tiles.ConveyorTile;
-import edu.chl.roborally.model.tiles.GameTile;
-import edu.chl.roborally.model.tiles.RotationTile;
+import edu.chl.roborally.model.tiles.*;
 
 /**
  * Created by axel on 2015-03-30.
@@ -62,9 +60,68 @@ public class VaultMap extends GameBoard {
         board[1][3] = new RotationTile(Constants.Directions.EAST);
         board[0][10] = new RotationTile(Constants.Directions.EAST);
 
+        //Pit
+        board[8][2] = new PitTile();
+        board[3][2] = new PitTile();
+        board[3][11] = new PitTile();
+        board[8][11] = new PitTile();
 
+        //Walls
+        board[2][0] = new WallTile(Constants.Directions.WEST);
+        board[4][0] = new WallTile(Constants.Directions.WEST);
+        board[7][0] = new WallTile(Constants.Directions.WEST);
+        board[9][0] = new WallTile(Constants.Directions.WEST);
 
+        board[0][2] = new WallTile(Constants.Directions.NORTH);
+        board[0][4] = new WallTile(Constants.Directions.NORTH);
+        board[0][7] = new WallTile(Constants.Directions.NORTH);
+        board[0][9] = new WallTile(Constants.Directions.NORTH);
+
+        board[11][2] = new WallTile(Constants.Directions.SOUTH);
+        board[11][4] = new WallTile(Constants.Directions.SOUTH);
+        board[11][7] = new WallTile(Constants.Directions.SOUTH);
+        board[11][9] = new WallTile(Constants.Directions.SOUTH);
+
+        board[2][11] = new WallTile(Constants.Directions.WEST);
+        board[4][11] = new WallTile(Constants.Directions.WEST);
+        board[7][11] = new WallTile(Constants.Directions.WEST);
+        board[9][11] = new WallTile(Constants.Directions.WEST);
+
+        board[12][2] = new WallTile(Constants.Directions.NORTH);
+        board[12][4] = new WallTile(Constants.Directions.NORTH);
+        board[12][7] = new WallTile(Constants.Directions.NORTH);
+        board[12][9] = new WallTile(Constants.Directions.NORTH);
+
+        board[15][2] = new WallTile(Constants.Directions.SOUTH);
+        board[15][4] = new WallTile(Constants.Directions.SOUTH);
+        board[15][7] = new WallTile(Constants.Directions.SOUTH);
+        board[15][9] = new WallTile(Constants.Directions.SOUTH);
+
+        board[14][1] = new WallTile(Constants.Directions.WEST);
+        board[14][3] = new WallTile(Constants.Directions.WEST);
+        board[14][5] = new WallTile(Constants.Directions.WEST);
+        board[14][6] = new WallTile(Constants.Directions.WEST);
+        board[14][7] = new WallTile(Constants.Directions.WEST);
+        board[14][9] = new WallTile(Constants.Directions.WEST);
+        board[14][11] = new WallTile(Constants.Directions.WEST);
+
+        board[5][3] = new WallTile(Constants.Directions.EAST);
+        board[6][3] = new WallTile(Constants.Directions.EAST);
+        board[5][9] = new WallTile(Constants.Directions.WEST);
+        board[6][9] = new WallTile(Constants.Directions.WEST);
+
+        board[2][5] = new WallTile(Constants.Directions.SOUTH);
+        board[2][6] = new WallTile(Constants.Directions.SOUTH);
+        board[8][5] = new WallTile(Constants.Directions.SOUTH);
+        board[8][6] = new WallTile(Constants.Directions.SOUTH);
+
+        board[2][3] = new WallTile(Constants.Directions.EAST);
+
+        //Two Directions Wall
+        board[4][4] = new WallTile(Constants.Directions.WEST, Constants.Directions.NORTH);
+        board[7][4] = new WallTile(Constants.Directions.WEST, Constants.Directions.SOUTH);
+        board[4][7] = new WallTile(Constants.Directions.EAST, Constants.Directions.NORTH);
+        board[7][7] = new WallTile(Constants.Directions.EAST, Constants.Directions.SOUTH);
 
     }
-
 }
