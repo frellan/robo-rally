8e9d21d7aa02d673c41f6fb4b7ccf7d3a1e44c2a
Aringskog <a.aringskog@gmail.com>
2015-04-22 13:31:59 +0200

    added actionlistener

--- src/main/java/edu/chl/roborally/view/StartScreen.java
+++ src/main/java/edu/chl/roborally/view/StartScreen.java
@@ -1,21 +1,45 @@
 package edu.chl.roborally.view;
 
+import edu.chl.roborally.controller.Controller;
+import edu.chl.roborally.model.RoboRally;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by fredrikkindstrom on 22/04/15.
  */
-public class StartScreen extends JPanel {
+public class StartScreen extends JPanel implements ActionListener{
+
+    private Controller controller;
+
 
     // Buttons
 
     private JButton newGameButton;
 
-    public StartScreen() {
+    public StartScreen(Controller controller) {
+        this.controller = controller;
+
         setLayout(new FlowLayout());
         newGameButton = new JButton("New Game");
         add(newGameButton);
+
+        newGameButton.addActionListener(this);
+
         setVisible(true);
+    }
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == "New Game"){
+            controller.newGame();
+        }
     }
 }
