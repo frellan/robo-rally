f4744307e61eec4c0b322f49fc576fa4fddbb179
Henrik Nilson <henrik.nilson91@gmail.com>
2015-04-30 11:52:25 +0200

    test init for pär

---
+++ src/test/java/edu/chl/roborally/model/cards/BackupCardTest.java
@@ -0,0 +1,22 @@
+package edu.chl.roborally.model.cards;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by henriknilson on 30/04/15.
+ */
+public class BackupCardTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testDoAction() throws Exception {
+
+    }
+}
