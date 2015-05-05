a7581cf5399e5f4be5e38171b22f1847b99e7842
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-05-05 14:03:21 +0200

    getting a bit closer

--- src/main/java/edu/chl/roborally/model/tiles/GameTile.java
+++ src/main/java/edu/chl/roborally/model/tiles/GameTile.java
@@ -13,6 +13,7 @@ public abstract class GameTile {
     protected static final int WITDH = 80;
     protected static final int HEIGHT = 80;
     protected BufferedImage icon;
+    protected Color color;
 
     public GameTile() {
         icon = new BufferedImage(WITDH,HEIGHT,BufferedImage.TYPE_INT_RGB);
@@ -24,5 +25,9 @@ public abstract class GameTile {
 
     public void draw() {
         Graphics2D g = (Graphics2D)icon.getGraphics();
+        color = Color.ORANGE;
+        g.setColor(color);
+        g.fillRect(0,0,WITDH,HEIGHT);
+        g.drawImage(icon,0,0,null);
     }
 }
