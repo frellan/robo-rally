0e3f2490c6a3d09c8cb05cd28301756f29c35312
Aringskog <a.aringskog@gmail.com>
2015-05-06 14:34:39 +0200

    created CardView and GamePanel

---
+++ src/main/java/edu/chl/roborally/view/gui/CardView.java
@@ -0,0 +1,14 @@
+package edu.chl.roborally.view.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by axel on 2015-05-06.
+ */
+public class CardView extends JPanel {
+
+    public CardView(){
+        setSize(1000, 228);
+
+    }
+}
