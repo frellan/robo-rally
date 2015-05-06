e036085879dbca563d4ab37fff004de9ac9a8e7d
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-05-06 10:22:19 +0200

    started using grid layout

--- src/main/java/edu/chl/roborally/view/gui/GameView.java
+++ src/main/java/edu/chl/roborally/view/gui/GameView.java
@@ -3,8 +3,6 @@ package edu.chl.roborally.view.gui;
 import edu.chl.roborally.model.Constants;
 import edu.chl.roborally.model.RoboRally;
 import edu.chl.roborally.model.maps.GameBoard;
-import edu.chl.roborally.model.tiles.BlankTile;
-import edu.chl.roborally.model.tiles.GameTile;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,14 +20,14 @@ public class GameView extends JPanel {
     public GameView(RoboRally model) {
         this.model = model;
         board = model.getGameBoard();
+        setLayout(new GridLayout(Constants.NUM_ROWS,Constants.NUM_COLS));
         setSize(width,height);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        drawGrid(g);
-        drawTiles(g);
+        placeTiles();
     }
 
     private void drawGrid(Graphics g) {
@@ -51,15 +49,11 @@ public class GameView extends JPanel {
         }
     }
 
-    private void drawTiles(Graphics g) {
-        int x = 0;
+    private void placeTiles() {
         for (int i = 0; i < Constants.NUM_COLS; i++) {
-            int y = 0;
             for(int j = 0; j < Constants.NUM_ROWS; j++) {
-                board.getTile(i,j).paintComponent(g);
-                y += Constants.TILE_SIZE;
+                add(board.getTile(i,j));
             }
-            x += Constants.TILE_SIZE;
         }
     }
 }
