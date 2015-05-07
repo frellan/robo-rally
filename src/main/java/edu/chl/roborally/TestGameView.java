338b20ecbf32184e66ae64b0cecf7f540e615886
feggak <fredrik.kindstrom@gmail.com>
2015-05-07 18:01:45 +0200

    added layout textures (testing)

--- src/main/java/edu/chl/roborally/TestGameView.java
+++ src/main/java/edu/chl/roborally/TestGameView.java
@@ -3,6 +3,7 @@ package edu.chl.roborally;
 import edu.chl.roborally.model.Player;
 import edu.chl.roborally.model.RoboRally;
 import edu.chl.roborally.model.maps.*;
+import edu.chl.roborally.view.gui.GamePanel;
 import edu.chl.roborally.view.gui.GameView;
 
 import javax.swing.*;
@@ -23,12 +24,14 @@ public class TestGameView {
         players.add(player2);
         RoboRally model = new RoboRally(players,map);
         JFrame frame = new JFrame();
-        GameView test = new GameView(model);
+        GamePanel main = new GamePanel(model);
+        //GameView test = new GameView(model);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setSize(1000, 720);
         frame.setLayout(new BorderLayout());
-        frame.add(test);
+        frame.add(main);
+        //frame.add(test);
         frame.setVisible(true);
     }
 }
--- src/main/java/edu/chl/roborally/view/gui/GamePanel.java
+++ src/main/java/edu/chl/roborally/view/gui/GamePanel.java
@@ -2,24 +2,40 @@ package edu.chl.roborally.view.gui;
 
 import edu.chl.roborally.model.RoboRally;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by axel on 2015-05-06.
  */
 public class GamePanel extends JPanel {
 
+    private BufferedImage bg;
+    private BufferedImage bgText;
     private JPanel gameView;
     private JPanel cardView;
 
     public GamePanel(RoboRally model){
-        gameView = new GameView(model);
-        cardView = new CardView(model);
 
+        try {
+            bg = ImageIO.read(this.getClass().getClassLoader().getResource("game_background.png"));
+            bgText = ImageIO.read(this.getClass().getClassLoader().getResource("game_background_text.png"));
+        } catch (java.io.IOException | NullPointerException e){
+            System.out.println("Images could not be read");
+        }
+
+        setBackground(Color.WHITE);
         setLayout(new FlowLayout());
         setSize(1000,700);
-        add(gameView);
-        add(cardView);
+    }
+
+    // Draw background
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
+        g.drawImage(bgText, 0, 0, getWidth(), getHeight(), this);
     }
 }
