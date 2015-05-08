cd75749d67df570115d1f6c92eddd2f0d9105068
Pertta <parlofqvist@gmail.com>
2015-05-08 12:13:12 +0200

    moved more tests

--- src/test/java/edu/chl/roborally/model/cards/MoveOneCardTest.java
+++ src/test/java/edu/chl/roborally/model/cards/MoveOneCardTest.java
@@ -1,7 +1,7 @@
-package edu.chl.roborally.model.gameactions;
+package edu.chl.roborally.model.cards;
 
-import edu.chl.roborally.utilities.Constants;
 import edu.chl.roborally.model.Player;
+import edu.chl.roborally.utilities.Constants;
 import edu.chl.roborally.utilities.Position;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,29 +9,25 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by henriknilson on 27/04/15.
+ * Created by Pertta on 15-04-30.
  */
-public class MoveAndBackUpPlayerTest {
+public class MoveOneCardTest {
 
     private Player player;
+    private MoveOneCard card;
 
     @Before
     public void setUp() throws Exception {
-        player = new Player(0, "MoveAndBackUpPlayer");
+        player = new Player(0, "cardMoveOnePlayer");
         player.setPosition(new Position(4,6));
         player.setDirection(Constants.Directions.NORTH);
         System.out.println(player.getName() + " starts at position " + player.getPosition());
     }
 
     @Test
-    public void testMovePlayer() throws Exception {
-        new MovePlayer(player);
+    public void testDoAction() throws Exception {
+        card = new MoveOneCard(10,false);
+        card.doAction(player);
         assertTrue(player.getPosition().getY() == 7 && player.getPosition().getX() == 4);
-    }
-
-    @Test
-    public void testBackUpPlayer() throws Exception {
-        new BackUpPlayer(player);
-        assertTrue(player.getPosition().getY() == 5 && player.getPosition().getX() == 4);
     }
 }
---
+++ src/test/java/edu/chl/roborally/model/cards/MoveTwoCardTest.java
@@ -0,0 +1,34 @@
+package edu.chl.roborally.model.cards;
+
+import edu.chl.roborally.model.Player;
+import edu.chl.roborally.utilities.Constants;
+import edu.chl.roborally.utilities.Position;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Pertta on 15-04-30.
+ */
+public class MoveTwoCardTest {
+
+    private Player player;
+    private MoveTwoCard card;
+
+    @Before
+    public void setUp() throws Exception {
+        player = new Player(0, "cardMoveTwoPlayer");
+        player.setPosition(new Position(4,6));
+        player.setDirection(Constants.Directions.NORTH);
+        System.out.println(player.getName() + " starts at position " + player.getPosition());
+    }
+
+    @Test
+    public void testDoAction() throws Exception {
+        card = new MoveTwoCard(10,false);
+        card.doAction(player);
+        assertTrue(player.getPosition().getY() == 8 && player.getPosition().getX() == 4);
+
+    }
+}
