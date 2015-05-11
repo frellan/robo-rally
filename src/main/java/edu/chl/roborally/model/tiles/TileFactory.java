e9724959ed11272a10322f20dd469558e8c721bf
Pertta <parlofqvist@gmail.com>
2015-05-11 15:49:39 +0200

    Working with TileFactory, big switch coming up...

--- src/main/java/edu/chl/roborally/model/tiles/TileFactory.java
+++ src/main/java/edu/chl/roborally/model/tiles/TileFactory.java
@@ -1,5 +1,8 @@
 package edu.chl.roborally.model.tiles;
 
+import edu.chl.roborally.model.tiles.attributes.ConveyorAttribute;
+import edu.chl.roborally.utilities.Constants;
+
 /**
  * Created by Pertta on 15-05-11.
  */
@@ -24,10 +27,31 @@ public class TileFactory {
 
     public GameTile createTile(int tileNbr) {
         GameTile tile = new GameTile();
-        if(tileNbr == 0) {
-            return tile.addAttribute();
+        if(tileNbr == 11) {
+            tile.addAttribute(new ConveyorAttribute(Constants.Directions.NORTH));
         }
-        return new GameTile();
+        else if (tileNbr == 12) {
+            tile.addAttribute(new ConveyorAttribute((Constants.Directions.WEST)));
+        }
+        else if (tileNbr == 13) {
+            tile.addAttribute((new ConveyorAttribute(Constants.Directions.SOUTH)));
+        }
+        else if (tileNbr == 14) {
+            tile.addAttribute(new ConveyorAttribute(Constants.Directions.EAST));
+        }
+        else if (tileNbr == 15) {
+            tile.addAttribute(new ConveyorAttribute(Constants.Directions.NORTH_WEST));
+        }
+        else if (tileNbr == 16) {
+            tile.addAttribute(new ConveyorAttribute(Constants.Directions.NORTH_EAST));
+        }
+        else if (tileNbr == 17) {
+            tile.addAttribute(new ConveyorAttribute(Constants.Directions.SOUTH_WEST));
+        }
+        else if (tileNbr == 18) {
+            tile.addAttribute(new ConveyorAttribute(Constants.Directions.SOUTH_EAST));
+        }
+        return tile;
     }
 
 }
