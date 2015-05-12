85189d2ca501b560ddecb4a15175a46d8879eeec
Pertta <parlofqvist@gmail.com>
2015-05-12 14:12:46 +0200

    Added handling of TurnCons in MovePlayer

--- src/main/java/edu/chl/roborally/model/gameactions/MovePlayer.java
+++ src/main/java/edu/chl/roborally/model/gameactions/MovePlayer.java
@@ -32,11 +32,38 @@ public class MovePlayer extends GameAction {
                 break;
             case SOUTH:
                 super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY() - 1));
+                break;
             case EAST:
                 super.player.setPosition(new Position(super.player.getPosition().getX()+1, super.player.getPosition().getY()));
                 break;
             case WEST:
                 super.player.setPosition(new Position(super.player.getPosition().getX()-1, super.player.getPosition().getY()));
+                break;
+            case WEST_NORTH:
+                super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY()+1));
+                break;
+            case EAST_NORTH:
+                super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY()+1));
+                break;
+            case WEST_SOUTH:
+                super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY() - 1));
+                break;
+            case EAST_SOUTH:
+                super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY() - 1));
+                break;
+            case NORTH_EAST:
+                super.player.setPosition(new Position(super.player.getPosition().getX()+1, super.player.getPosition().getY()));
+                break;
+            case SOUTH_EAST:
+                super.player.setPosition(new Position(super.player.getPosition().getX()+1, super.player.getPosition().getY()));
+                break;
+            case NORTH_WEST:
+                super.player.setPosition(new Position(super.player.getPosition().getX()-1, super.player.getPosition().getY()));
+                break;
+            case SOUTH_WEST:
+                super.player.setPosition(new Position(super.player.getPosition().getX()-1, super.player.getPosition().getY()));
+                break;
+
         }
     }
 }
