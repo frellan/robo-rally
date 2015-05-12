2257bcb52b3cda317f9dab7c3f21e34c8266aff1
Pertta <parlofqvist@gmail.com>
2015-05-12 12:42:51 +0200

    More work on TurnCons

--- src/main/java/edu/chl/roborally/model/tiles/attributes/TurnConveyorAttribute.java
+++ src/main/java/edu/chl/roborally/model/tiles/attributes/TurnConveyorAttribute.java
@@ -2,6 +2,7 @@ package edu.chl.roborally.model.tiles.attributes;
 
 import edu.chl.roborally.model.Player;
 import edu.chl.roborally.model.Turn;
+import edu.chl.roborally.model.gameactions.MovePlayer;
 import edu.chl.roborally.utilities.Constants;
 
 import java.awt.*;
@@ -15,16 +16,37 @@ public class TurnConveyorAttribute implements Attribute {
 
     public TurnConveyorAttribute(Constants.Directions d) {
         this.d = d;
-
     }
 
     @Override
     public void doAction(Player player) {
-
+        new MovePlayer(player, d);
     }
 
     @Override
     public void draw(Graphics g, int x, int y) {
+        char[] message;
+        g.setColor(new Color(247,215,8));
+        g.fillRect(x, y, Constants.TILE_SIZE, Constants.TILE_SIZE);
+        g.setColor(Color.BLACK);
+        switch (d) {
+            case NORTH_WEST:
+                message = "CNW".toCharArray();
+                g.drawChars(message, 0, message.length, x, y + 10);
+                break;
+            case NORTH_EAST:
+                message = "CNE".toCharArray();
+                g.drawChars(message, 0, message.length, x, y + 10);
+                break;
+            case SOUTH_WEST:
+                message = "CSW".toCharArray();
+                g.drawChars(message, 0, message.length, x, y + 10);
+                break;
+            case SOUTH_EAST:
+                message = "CSE".toCharArray();
+                g.drawChars(message, 0, message.length, x, y + 10);
+                break;
+        }
 
     }
 }
