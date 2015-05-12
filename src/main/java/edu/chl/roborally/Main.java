ab4aba9cc8ef95dd2ba18d051f0d143412ecc336
Aringskog <a.aringskog@gmail.com>
2015-05-12 15:40:25 +0200

    created a progress-console

--- src/main/java/edu/chl/roborally/Main.java
+++ src/main/java/edu/chl/roborally/Main.java
@@ -1,6 +1,9 @@
 package edu.chl.roborally;
 
 import edu.chl.roborally.controller.AppController;
+import edu.chl.roborally.view.gui.ConsoleView;
+
+import javax.swing.*;
 
 public final class Main {
 
@@ -11,8 +14,13 @@ public final class Main {
 	}
 
 	public static void main(String[] args) {
-        new AppController();
+        //new AppController();
 		//new TestGameView();
+
+        new TestConsole();
+        for(int i = 0; i < 50; i ++){
+            System.out.println(i);
+        }
     }
 
 	private static String getOsName() {
