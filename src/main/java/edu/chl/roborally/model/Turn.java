54f2ebb8a4c713e74231aeb51a848df7c7ac7ffa
Aringskog <a.aringskog@gmail.com>
2015-05-18 13:59:35 +0200

    added UPDATE_STATUS event in turn

--- src/main/java/edu/chl/roborally/model/Turn.java
+++ src/main/java/edu/chl/roborally/model/Turn.java
@@ -37,7 +37,8 @@ public class Turn{
         executeBoardElements();
         fireLasers();
 
-        EventTram.getInstance().publish(EventTram.Event.UPDATE_GAMEBOARD, null);
+        EventTram.getInstance().publish(EventTram.Event.UPDATE_BOARD, null);
+        EventTram.getInstance().publish(EventTram.Event.UPDATE_STATUS, null);
     }
 
     // Executing methods
