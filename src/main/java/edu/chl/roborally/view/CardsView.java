f4ba3825deb799073f5ea45e374dc73b8921db61
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-05-19 15:21:50 +0200

    create view CardsView

---
+++ src/main/java/edu/chl/roborally/view/CardsView.java
@@ -0,0 +1,19 @@
+package edu.chl.roborally.view;
+
+import javax.swing.*;
+
+/**
+ * Created by fredrikkindstrom on 19/05/15.
+ */
+public class CardsView extends JPanel {
+
+    public CardsView() {
+        setLayout(null);
+        setSize(600,170);
+        createRegisterView();
+    }
+
+    private void createRegisterView() {
+        
+    }
+}
