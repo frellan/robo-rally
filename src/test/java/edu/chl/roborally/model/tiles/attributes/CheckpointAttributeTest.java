490309ac7812548197e820b5d6eaf61e28bca67b
Henrik Nilson <henrik.nilson91@gmail.com>
2015-05-19 17:24:39 +0200

    fixed robot errorsssss s

--- src/test/java/edu/chl/roborally/model/tiles/attributes/CheckpointAttributeTest.java
+++ src/test/java/edu/chl/roborally/model/tiles/attributes/CheckpointAttributeTest.java
@@ -4,6 +4,7 @@ import edu.chl.roborally.model.Player;
 import edu.chl.roborally.model.RoboRally;
 import edu.chl.roborally.model.maps.GameBoard;
 import edu.chl.roborally.model.maps.IslandKingMap;
+import edu.chl.roborally.model.robot.Robot;
 import edu.chl.roborally.utilities.Position;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,13 +24,13 @@ public class CheckpointAttributeTest {
 
     @Before
     public void setUp() throws Exception {
-        player = new Player(1,"Dave");
+        player = new Player(1,new Robot("Test Robot"));
         player.setCheckpointId(0);
         map = new IslandKingMap();
         ArrayList<Player> players = new ArrayList<>();
         players.add(player);
         model = new RoboRally(players,map);
-        System.out.println(player.getRobot() + " starts at position " + player.getPosition() + " and has checkpoint " + player.getCheckpoint());
+        System.out.println(player.getName() + " starts at position " + player.getPosition() + " and has checkpoint " + player.getCheckpoint());
     }
 
     @Test
