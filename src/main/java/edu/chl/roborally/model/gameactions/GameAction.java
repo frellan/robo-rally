2f134e46c53665606453b5cb4d1cd907ef0cccb0
Henrik Nilson <henrik.nilson91@gmail.com>
2015-05-26 11:52:25 +0200

    some important stuff

--- src/main/java/edu/chl/roborally/model/gameactions/GameAction.java
+++ src/main/java/edu/chl/roborally/model/gameactions/GameAction.java
@@ -6,6 +6,6 @@ import edu.chl.roborally.model.Player;
  * Created by henriknilson on 31/03/15.
  */
 public interface GameAction {
-    abstract void doAction(Player p);
+    void doAction(Player p);
 
 }
