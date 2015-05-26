6c552a3af5fd7692b0d588cc6da4a33e9d2a6ddf
Pertta <parlofqvist@gmail.com>
2015-05-26 11:41:35 +0200

    All actions converted to new system

--- src/main/java/edu/chl/roborally/model/gameactions/MovePlayer.java
+++ src/main/java/edu/chl/roborally/model/gameactions/MovePlayer.java
@@ -14,40 +14,40 @@ public class MovePlayer implements GameAction {
     public void action(Player p) {
         switch (p.getDirection()) {
             case NORTH:
-                super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY() - 1));
+                p.setPosition(new Position(p.getPosition().getX(), p.getPosition().getY() - 1));
                 break;
             case SOUTH:
-                super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY() + 1));
+                p.setPosition(new Position(p.getPosition().getX(), p.getPosition().getY() + 1));
                 break;
             case EAST:
-                super.player.setPosition(new Position(super.player.getPosition().getX() + 1, super.player.getPosition().getY()));
+                p.setPosition(new Position(p.getPosition().getX() + 1, p.getPosition().getY()));
                 break;
             case WEST:
-                super.player.setPosition(new Position(super.player.getPosition().getX() - 1, super.player.getPosition().getY()));
+                p.setPosition(new Position(p.getPosition().getX() - 1, p.getPosition().getY()));
                 break;
             case WEST_NORTH:
-                super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY() - 1));
+                p.setPosition(new Position(p.getPosition().getX(), p.getPosition().getY() - 1));
                 break;
             case EAST_NORTH:
-                super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY() - 1));
+                p.setPosition(new Position(p.getPosition().getX(), p.getPosition().getY() - 1));
                 break;
             case WEST_SOUTH:
-                super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY() + 1));
+                p.setPosition(new Position(p.getPosition().getX(), p.getPosition().getY() + 1));
                 break;
             case EAST_SOUTH:
-                super.player.setPosition(new Position(super.player.getPosition().getX(), super.player.getPosition().getY() + 1));
+                p.setPosition(new Position(p.getPosition().getX(), p.getPosition().getY() + 1));
                 break;
             case NORTH_EAST:
-                super.player.setPosition(new Position(super.player.getPosition().getX() + 1, super.player.getPosition().getY()));
+                p.setPosition(new Position(p.getPosition().getX() + 1, p.getPosition().getY()));
                 break;
             case SOUTH_EAST:
-                super.player.setPosition(new Position(super.player.getPosition().getX() + 1, super.player.getPosition().getY()));
+                p.setPosition(new Position(p.getPosition().getX() + 1, p.getPosition().getY()));
                 break;
             case NORTH_WEST:
-                super.player.setPosition(new Position(super.player.getPosition().getX() - 1, super.player.getPosition().getY()));
+                p.setPosition(new Position(p.getPosition().getX() - 1, p.getPosition().getY()));
                 break;
             case SOUTH_WEST:
-                super.player.setPosition(new Position(super.player.getPosition().getX() - 1, super.player.getPosition().getY()));
+                p.setPosition(new Position(p.getPosition().getX() - 1, p.getPosition().getY()));
                 break;
         }
         EventTram.getInstance().publish(EventTram.Event.EXECUTE_TILE_ACTION_BEFORE,p,null);
