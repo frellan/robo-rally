1a949fe9eb29fb7856bb60164b4da56d7ae4a92d
Pertta <parlofqvist@gmail.com>
2015-05-29 13:59:22 +0200

    LaserTesting

--- src/test/java/edu/chl/roborally/model/TurnTest.java
+++ src/test/java/edu/chl/roborally/model/TurnTest.java
@@ -20,14 +20,17 @@ public class TurnTest {
 
     private Player player1;
     private Player player2;
+    private Player player3;
     private RoboRally model;
     private GameBoard map;
     private ArrayList<Player> players;
 
     @Before
     public void setUp() throws Exception {
-        player1 = new Player(1,new Robot("Test LaserRobot", Color.ORANGE));
-        player2 = new Player(1,new Robot("Test Another LaserRobot", Color.ORANGE));
+        player1 = new Player(1,new Robot("Test LaserRobot1", Color.ORANGE));
+        player2 = new Player(1,new Robot("Test LaserRobot2", Color.ORANGE));
+        player3 = new Player(1,new Robot("Test LaserRobot3", Color.ORANGE));
+
         map = new IslandKingMap();
         players = new ArrayList<>();
         players.add(player1);
@@ -38,18 +41,25 @@ public class TurnTest {
     @Test
     public void testLaser() throws Exception {
         //Players on same X different Y
-        player1.setPosition(new Position(5,6));
-        player2.setPosition(new Position(1,6));
+        player1.setPosition(new Position(1,3));
+        player2.setPosition(new Position(1,4));
+        player3.setPosition(new Position(1,5));
         player1.setDirection(Constants.Directions.NORTH);
         player2.setDirection(Constants.Directions.NORTH);
+        player3.setDirection(Constants.Directions.NORTH);
         System.out.println("player1 has " + player1.getDamageTokens() + " damageTokens");
         System.out.println("player2 has " + player2.getDamageTokens() + " damageTokens");
+        System.out.println("player3 has " + player3.getDamageTokens() + " damageTokens");
+
         fireLasers();
         System.out.println("player1 has " + player1.getDamageTokens() + " damageTokens after LASER");
         System.out.println("player2 has " + player2.getDamageTokens() + " damageTokens after LASER");
+        System.out.println("player3 has " + player3.getDamageTokens() + " damageTokens after LASER");
 
-        assertTrue(player1.getDamageTokens() == 0);
-        assertTrue(player2.getDamageTokens() == 0);
+
+        assertTrue(player1.getDamageTokens() == 1);
+        assertTrue(player2.getDamageTokens() == 1);
+        assertTrue(player3.getDamageTokens() == 0);
 
         //assertTrue(player1.getDamageTokens() == 0);
         //assertTrue(player2.getDamageTokens() == 0);
@@ -58,52 +68,43 @@ public class TurnTest {
     private void fireLasers() {
         // Loop all players, all players fire lasers in their direction
         //TODO Stop laser if wall_tile in that direction
+        //TODO Stop when other player is hit
         for (Player p : players) {
             //Get current laser power for the player
             int playerLaserPower = p.getLaserPower();
             switch (p.getDirection()) {
                 case NORTH:
-                    //If x is equal and y is smaller
-                    //Calculate how many tiles to loop through
-
-                    for (Player enemy : players) {
-                        if (!p.equals(enemy)) {
-                            if (enemy.getPosition().getX() == p.getPosition().getX() && enemy.getPosition().getY() < p.getPosition().getY()) {
-                                    enemy.takeDamage(playerLaserPower);
-                                    break;
-                            }
-                        }
-                    }
-                case SOUTH:
                     //If x is equal and y is bigger
                     for (Player enemy : players) {
-                        if (!p.equals(enemy)) {
-                            if (enemy.getPosition().getX() == p.getPosition().getX() && enemy.getPosition().getY() > p.getPosition().getY()) {
-                                enemy.takeDamage(playerLaserPower);
-                                break;
-                            }
+                        if (enemy.getPosition().getX() == p.getPosition().getX() && enemy.getPosition().getY() > p.getPosition().getY()) {
+                            enemy.takeDamage(playerLaserPower);
                         }
                     }
+                    break;
+                case SOUTH:
+                    //If x is equal and y is smaller
+                    for (Player enemy : players) {
+                        if (enemy.getPosition().getX() == p.getPosition().getX() && enemy.getPosition().getY() < p.getPosition().getY()) {
+                            enemy.takeDamage(playerLaserPower);
+                        }
+                    }
+                    break;
                 case EAST:
                     //If y is equal and x is bigger
                     for (Player enemy : players) {
-                        if (!p.equals(enemy)) {
-                            if (enemy.getPosition().getY() == p.getPosition().getY() && enemy.getPosition().getX() > p.getPosition().getX()) {
-                                enemy.takeDamage(playerLaserPower);
-                                break;
-                            }
+                        if (enemy.getPosition().getY() == p.getPosition().getY() && enemy.getPosition().getX() > p.getPosition().getX()) {
+                            enemy.takeDamage(playerLaserPower);
                         }
                     }
+                    break;
                 case WEST:
                     //If y is equal and x is smaller
                     for (Player enemy : players) {
-                        if (!p.equals(enemy)) {
-                            if (enemy.getPosition().getY() == p.getPosition().getY() && enemy.getPosition().getX() < p.getPosition().getX()) {
-                                enemy.takeDamage(playerLaserPower);
-                                break;
-                            }
+                        if (enemy.getPosition().getY() == p.getPosition().getY() && enemy.getPosition().getX() < p.getPosition().getX()) {
+                            enemy.takeDamage(playerLaserPower);
                         }
                     }
+                    break;
             }
         }
     }
